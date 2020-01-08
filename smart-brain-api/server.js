6c17d4c5aa385529f7f7d9fbc850bb4970fa24aa
cspela <spela.cucko@gmail.com>
2020-01-08 18:48:28 +0100

    Adding transaction & updating register, signin enpoints

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -42,27 +42,40 @@ app.get('/profile/:id', (req,res) => {
 		.from('users')
 		.where('id', id)
 		.then(user => {
-			if(!user[0]){
-				res.status(400).json(`Can't get user`); 
+			if(user.length){
+				res.json(user[0]);
+			}else{
+				res.status(400).json(`Not found.`); 
 			}
-			res.json(user[0]);
 		}).catch(err => {
-			res.status(400).json(`Can't get user`); 
+			res.status(400).json(`Error getting user`); 
 		})
 })
 
-//INSERT INTO LOGIN!!!!!
 app.post('/register', (req,res) => {
 	let exists = false; 
 	const { name, email, password } = req.body; 
-	db('users')
-      .returning('*')
-	  .insert({
-		name: name, 
-		email: email, 
-		joined: new Date()
-	}).then(user => {
-		res.json(user[0]); 
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail =>{
+			return trx('users')
+		      .returning('*')
+			  .insert({
+				name: name, 
+				email: loginEmail[0], 
+				joined: new Date()
+			}).then(user => {
+				res.json(user[0]); 
+			})	
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
 	}).catch(err => {
 		res.status(400).json(`Unable to register`); 
 	})
@@ -70,18 +83,20 @@ app.post('/register', (req,res) => {
 
 app.post('/signin', (req,res) => {
 	const { email, password } = req.body; 
-	db.select('*').from('login')
-		.where({email: email, hash: password})
-		.then(user => {
-			if(!user[0]){
+	db.select('email', 'hash')
+		.from('login')
+		.where('email', email)
+		.then(data => {
+			if(bcrypt.compareSync(password, data[0].hash)){
+				db.select('*').from('users').where('email', data[0].email)
+				.then(user => {
+					res.json(user[0])
+				}).catch(err => {
+					res.status(400).json(`Can't sign in`); 
+				})
+			}else{
 				res.status(400).json(`Can't sign in`); 
 			}
-			db.select('*')
-				.from('users')
-				.where('email', email)
-				.then(user => {
-					res.json(user[0]);
-				}) 
 		}).catch(err => {
 			res.status(400).json(`Can't sign in`); 
 		})
@@ -91,12 +106,12 @@ app.put('/image', (req, res) => {
 	const { id } = req.body; 
 	db('users')
 		.where('id', id)
-		.increment({'entries': 1})
+		.increment('entries', 1)
 		.returning('entries')
 		.then(entries => {
 			res.json(entries); 
 		}).catch(err => {
-			res.status(400).json(`Can't update entries`)
+			res.status(400).json(`Unable to get entries`)
 		})
 })
 
